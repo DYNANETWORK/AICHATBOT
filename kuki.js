cb87047d5eaff550a38903e1dc1f80a67e32daaa
SUKHPAL <86770159+SUKHPAL443@users.noreply.github.com>
2022-03-01 14:20:26 +0530

    Update kuki.js

--- kuki.js
+++ kuki.js
@@ -13,6 +13,14 @@ var text = message.text
  })
 if (message.text.toLowerCase()=='/start'){
         slimbot.sendMessage(message.chat.id, "I am kuki");
+if (message.text.toLowerCase()=='hello'){
+        slimbot.sendMessage(message.chat.id, "Hi Dear how are you");
+if (message.text.toLowerCase()=='I am cool"){
+        slimbot.sendMessage(message.chat.id, "Wow Dear nyc to hear");
+if (message.text.toLowerCase()== 'Where are you from'){
+        slimbot.sendMessage(message.chat.id, "DEAR FIRSTLY I AM A PROGRAM. I AM NOT HUMAN.I AM A ARTIFICIAL INTELLIGENCE BOT AND I AM PROGRAMMED BY OUR OWNER @SUKHPAL_KHERERA.");
+  
+                          
     }
 });
 
